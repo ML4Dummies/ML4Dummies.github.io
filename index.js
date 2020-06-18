fbb3d17cb4bc838b9f443e219aebe2585bf05ab7
aasokan <aasokan@gitlab.eecs.umich.edu>
2020-06-18 12:27:30 -0400

    data loading complete

--- index.js
+++ index.js
@@ -2,9 +2,67 @@ console.log('Hello TensorFlow');
 
 const trainingFile = document.getElementById('fileuploadTrain');
 
+function displayHTMLTable(results){
+	var table = "<table class='table'>";
+	var data = results.data;
+	 
+	for(i=0;i<data.length;i++){
+		table+= "<tr>";
+		var row = data[i];
+		var cells = row.join(",").split(",");
+		 
+		for(j=0;j<cells.length;j++){
+			table+= "<td>";
+			table+= cells[j];
+			table+= "</th>";
+		}
+		table+= "</tr>";
+	}
+  table+= "</table>";
+  console.log("test");
+  document.querySelector('#console').innerHTML = table;
+	// $("#parsed_csv_list").html(table);
+}
+
+function convertToMatrix(results){
+  var data = results.data;
+  // console.log(data.length)
+  var matrix=new Array();
+  for(i=1;i<data.length-1;i++){
+    var row = data[i];
+    var cells = row.map(Number);
+    //console.log(cells)
+    matrix.push(cells);
+  }
+  console.log(matrix);
+}
+
 trainingFile.addEventListener("change", handleFiles, false);
 function handleFiles() {
   const file = this.files; /* now you can work with the file list */
-  console.log(trainingFile)
-
+  
+  $('#fileuploadTrain').parse({
+    config: {
+      delimiter: ",",
+      header:false, //Handled in the convertToMatrix
+      complete: convertToMatrix
+      //console.log//displayHTMLTable,
+    },
+    before: function(file, inputElem)
+    {
+      //console.log("Parsing file...", file);
+    },
+    error: function(err, file)
+    {
+      //console.log("ERROR:", err, file);
+    },
+    complete: function()
+    {
+      console.log("Done with all files");
+    }
+  });
+  
+  console.log(trainingFile);
 }
+
+
